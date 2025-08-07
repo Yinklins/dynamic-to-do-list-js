12f279075e447db9deaa754e8d56d2dd743a6313
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-08-07 22:21:42 +0100

    modify script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Setup Event listener for page load 
 document.addEventListener('DOMContentLoaded', () => {
 // select Dom Elements
-const addButton = document.getElementById('add-task');
+const addButton = document.getElementById('add-task-btn');
 const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
 
